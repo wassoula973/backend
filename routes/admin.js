1144b689733c15aaab355b89f117e6fbbab479e5
marwa665 <tarhounimarwa171@gmail.com>
2025-03-01 00:38:35 +0100

    ay message

--- routes/admin.js
+++ routes/admin.js
@@ -8,9 +8,10 @@ adminRouter.get("/users", [isConnected], async (request, response) => {
   const users = await User.find();
   response.send(users);
 });
-
-adminRouter.get("/:id marwa", [isConnected], async (request, response) => {
+adminRouter.get("/:id", [isConnected], async (request, response) => {
   const user = await User.findById(request.params.id);
+  console.log("trah 9olli weslek changement ou nn ");
+
   if (user) {
     response.send(user);
   } else response.status(404).send("not found");
