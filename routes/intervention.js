5b8012300b1893b43bbcb7c1427fed46eefd60df
wissal <wissalyounssi2@gmail.com>
2025-03-09 15:20:53 +0100

    le 09/03

--- routes/intervention.js
+++ routes/intervention.js
@@ -1,6 +1,28 @@
 const { Router } = require("express");
 const Intervention = require("../models/intervention");
 const { isConnected } = require("../middlewares");
+const multer = require("multer");
+const nodemailer = require("nodemailer");
+const User = require("../models/user");
+
+const transport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "younssiwissal3@gmail.com",
+    pass: "kiapzllzosqjpvxe",
+  },
+});
+
+var storage = multer.diskStorage({
+  destination: "./images",
+  filename: (request, image, cb) => {
+    const extension =
+      image.originalname.split(".")[image.originalname.split(".").length - 1];
+    cb(null, image.originalname + "-" + Date.now() + "." + extension);
+  },
+});
+
+var upload = multer({ storage });
 
 const interventionRouter = Router();
 
@@ -10,30 +32,72 @@ interventionRouter.get("/", [isConnected], async (request, response) => {
 });
 
 interventionRouter.get("/:id", [isConnected], async (request, response) => {
-  const intervention = await Intervention.findById(request.params.id);
+  const intervention = await Intervention.findById(request.params.id).populate([
+    "gerant",
+    "station",
+  ]);
   if (intervention) {
     response.send(intervention);
   } else response.status(404).send("not found");
 });
 
-interventionRouter.post("/", [isConnected], (request, response) => {
-  const { gerant, station, error, intensity } = request.body;
-
-  const intervention = new Intervention({
-    gerant,
-    station,
-    error,
-    intensity,
-  });
-  intervention
-    .save()
-    .then((savedIntervention) => {
-      response.send(savedIntervention);
-    })
-    .catch((error) => {
-      response.status(500).send(error);
+interventionRouter.get(
+  "/gerant/:id",
+  [isConnected],
+  async (request, response) => {
+    const interventions = await Intervention.find({
+      gerant: request.params.id,
     });
-});
+    response.send(interventions);
+  }
+);
+
+interventionRouter.get(
+  "/assistant/:id",
+  [isConnected],
+  async (request, response) => {
+    const user = await User.findById(request.params.id);
+    const interventions = await Intervention.find({
+      "station.gouvernorat": "Tunis",
+    }).populate(["gerant", "station"]);
+    response.send(interventions);
+  }
+);
+
+interventionRouter.post(
+  "/",
+  [isConnected, upload.single("image")],
+  (request, response) => {
+    const { gerant, station, error, intensity } = request.body;
+    const imageName = request.file ? request.file.filename : "";
+    const intervention = new Intervention({
+      gerant,
+      station,
+      error,
+      intensity,
+      image: imageName,
+    });
+    intervention
+      .save()
+      .then(async (savedIntervention) => {
+        const g = await User.findById(gerant);
+
+        const contenu = {
+          from: process.env.nodemailer_email,
+          to: g.email,
+          subject: "Ticket Id",
+          html: "Ticket id : " + savedIntervention._id,
+        };
+        transport.sendMail(contenu, (error, mail) => {
+          console.log({ error, mail });
+        });
+        response.send(savedIntervention);
+      })
+      .catch((error) => {
+        response.status(500).send(error);
+      });
+  }
+);
 
 interventionRouter.put("/:id", [isConnected], async (request, response) => {
   const { etat, technicien, intensity } = request.body;
